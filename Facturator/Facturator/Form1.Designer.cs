b658232642da9693d3685ede987bedaad36a4491
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2016-11-02 18:48:12 +0100

    base: Add basic interface
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

--- Facturator/Facturator/Form1.Designer.cs
+++ Facturator/Facturator/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Facturator
 {
-    partial class Form1
+    partial class Facturacion
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,12 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.calcularButton = new System.Windows.Forms.Button();
+            this.p1quantity = new System.Windows.Forms.TextBox();
+            this.p2quantity = new System.Windows.Forms.TextBox();
+            this.p3quantity = new System.Windows.Forms.TextBox();
+            this.p1label = new System.Windows.Forms.Label();
+            this.p2label = new System.Windows.Forms.Label();
+            this.p3label = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalPriceLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calcularButton
+            // 
+            this.calcularButton.Location = new System.Drawing.Point(29, 211);
+            this.calcularButton.Name = "calcularButton";
+            this.calcularButton.Size = new System.Drawing.Size(75, 23);
+            this.calcularButton.TabIndex = 0;
+            this.calcularButton.Text = "Calcular";
+            this.calcularButton.UseVisualStyleBackColor = true;
+            this.calcularButton.Click += new System.EventHandler(this.calcularButton_Click);
+            // 
+            // p1quantity
+            // 
+            this.p1quantity.Location = new System.Drawing.Point(172, 68);
+            this.p1quantity.Name = "p1quantity";
+            this.p1quantity.Size = new System.Drawing.Size(100, 20);
+            this.p1quantity.TabIndex = 1;
+            this.p1quantity.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // p2quantity
+            // 
+            this.p2quantity.Location = new System.Drawing.Point(172, 110);
+            this.p2quantity.Name = "p2quantity";
+            this.p2quantity.Size = new System.Drawing.Size(100, 20);
+            this.p2quantity.TabIndex = 2;
+            this.p2quantity.TextChanged += new System.EventHandler(this.p2quantity_TextChanged);
+            // 
+            // p3quantity
+            // 
+            this.p3quantity.Location = new System.Drawing.Point(172, 154);
+            this.p3quantity.Name = "p3quantity";
+            this.p3quantity.Size = new System.Drawing.Size(100, 20);
+            this.p3quantity.TabIndex = 3;
+            this.p3quantity.TextChanged += new System.EventHandler(this.p3quantity_TextChanged);
+            // 
+            // p1label
+            // 
+            this.p1label.AutoSize = true;
+            this.p1label.Location = new System.Drawing.Point(26, 75);
+            this.p1label.Name = "p1label";
+            this.p1label.Size = new System.Drawing.Size(35, 13);
+            this.p1label.TabIndex = 4;
+            this.p1label.Text = "label1";
+            this.p1label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // p2label
+            // 
+            this.p2label.AutoSize = true;
+            this.p2label.Location = new System.Drawing.Point(26, 117);
+            this.p2label.Name = "p2label";
+            this.p2label.Size = new System.Drawing.Size(35, 13);
+            this.p2label.TabIndex = 5;
+            this.p2label.Text = "label2";
+            this.p2label.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // p3label
+            // 
+            this.p3label.AutoSize = true;
+            this.p3label.Location = new System.Drawing.Point(26, 161);
+            this.p3label.Name = "p3label";
+            this.p3label.Size = new System.Drawing.Size(35, 13);
+            this.p3label.TabIndex = 6;
+            this.p3label.Text = "label3";
+            this.p3label.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(169, 216);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(34, 13);
+            this.totalLabel.TabIndex = 7;
+            this.totalLabel.Text = "Total:";
+            // 
+            // totalPriceLabel
+            // 
+            this.totalPriceLabel.AutoSize = true;
+            this.totalPriceLabel.Location = new System.Drawing.Point(242, 216);
+            this.totalPriceLabel.Name = "totalPriceLabel";
+            this.totalPriceLabel.Size = new System.Drawing.Size(30, 13);
+            this.totalPriceLabel.TabIndex = 8;
+            this.totalPriceLabel.Text = "price";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Producto";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(169, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Cantidad";
+            // 
+            // Facturacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.totalPriceLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.p3label);
+            this.Controls.Add(this.p2label);
+            this.Controls.Add(this.p1label);
+            this.Controls.Add(this.p3quantity);
+            this.Controls.Add(this.p2quantity);
+            this.Controls.Add(this.p1quantity);
+            this.Controls.Add(this.calcularButton);
+            this.Name = "Facturacion";
+            this.Text = "Facturacion";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button calcularButton;
+        private System.Windows.Forms.TextBox p1quantity;
+        private System.Windows.Forms.TextBox p2quantity;
+        private System.Windows.Forms.TextBox p3quantity;
+        private System.Windows.Forms.Label p1label;
+        private System.Windows.Forms.Label p2label;
+        private System.Windows.Forms.Label p3label;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label totalPriceLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Facturator/Facturator/Form1.cs
+++ Facturator/Facturator/Form1.cs
@@ -10,11 +10,51 @@ using System.Windows.Forms;
 
 namespace Facturator
 {
-    public partial class Form1 : Form
+    public partial class Facturacion : Form
     {
-        public Form1()
+        public Facturacion()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p2quantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void p3quantity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcularButton_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
