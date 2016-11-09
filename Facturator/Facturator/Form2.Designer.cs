55cdfaad01c48e7236b74dd0afdc34045a75f70c
Álvaro Orduna León <alvaro.orduna.92@gmail.com>
2016-11-09 19:00:03 +0100

    Terminando la app
    Signed-off-by: Álvaro Orduna León <alvaro.orduna.92@gmail.com>

---
+++ Facturator/Facturator/Form2.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Facturator
+{
+    partial class UserAuth
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.user = new System.Windows.Forms.TextBox();
+            this.pass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.submit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.errorMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(101, 51);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(137, 20);
+            this.user.TabIndex = 0;
+            // 
+            // pass
+            // 
+            this.pass.Location = new System.Drawing.Point(101, 100);
+            this.pass.Name = "pass";
+            this.pass.Size = new System.Drawing.Size(137, 20);
+            this.pass.TabIndex = 1;
+            this.pass.TextChanged += new System.EventHandler(this.pass_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Autentificación";
+            // 
+            // submit
+            // 
+            this.submit.Location = new System.Drawing.Point(163, 140);
+            this.submit.Name = "submit";
+            this.submit.Size = new System.Drawing.Size(75, 23);
+            this.submit.TabIndex = 3;
+            this.submit.Text = "Entrar";
+            this.submit.UseVisualStyleBackColor = true;
+            this.submit.Click += new System.EventHandler(this.submit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 103);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Contraseña";
+            // 
+            // errorMsg
+            // 
+            this.errorMsg.AutoSize = true;
+            this.errorMsg.Location = new System.Drawing.Point(12, 145);
+            this.errorMsg.Name = "errorMsg";
+            this.errorMsg.Size = new System.Drawing.Size(0, 13);
+            this.errorMsg.TabIndex = 6;
+            // 
+            // UserAuth
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(259, 186);
+            this.Controls.Add(this.errorMsg);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.submit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pass);
+            this.Controls.Add(this.user);
+            this.Name = "UserAuth";
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.TextBox pass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button submit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label errorMsg;
+    }
+}
---
+++ Facturator/Facturator/Form2.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Facturator
+{
+    public partial class UserAuth : Form
+    {
+        Dictionary<string, string> users = new Dictionary<string, string>();
+
+        public UserAuth()
+        {
+            InitializeComponent();
+
+            users.Add("Joseba", "1234");
+            users.Add("Josu", "4321");
+            users.Add("Álvaro", "alvaro");
+            users.Add("Iñigo", "123456789");
+
+            pass.Text = "";
+            pass.PasswordChar = '*';
+            pass.MaxLength = 14;
+
+            errorMsg.ForeColor = System.Drawing.Color.Red;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+            String userName = user.Text;
+            String userPass = pass.Text;
+
+            if (!userName.Equals("") && !userPass.Equals("")) {
+                String userPassFormDic;
+                if (users.TryGetValue(userName,out userPassFormDic) &&
+                    userPassFormDic.Equals(userPass))
+                {
+                    Facturacion f = new Facturacion();
+
+                    errorMsg.Text = "";
+
+                    this.Hide();
+                    f.Show();
+                    return;
+                }
+            }
+
+            errorMsg.Text = "Error de autentificación";
+        }
+
+        private void pass_TextChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
